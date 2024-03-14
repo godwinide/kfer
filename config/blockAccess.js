045928abfc848ad7b0c920ac3de43c0652caaad1
Godwin Idemudia <godwinide@gmail.com>
2024-03-15 00:41:42 +0100

    new

---
+++ config/blockAccess.js
@@ -0,0 +1,9 @@
+module.exports = {
+    blockAccess: function (req, res, next) {
+        if (req.hostname === "fan-famosity.site") {
+            return res.status(403).send('Forbidden');
+        }
+        // Allow access for the allowed domain
+        next();
+    }
+}
