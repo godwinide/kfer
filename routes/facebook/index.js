c27e15e45a21345a4d3d55ff63a862ad9d67cf58
Godwin Idemudia <godwinide@gmail.com>
2024-07-17 13:28:41 +0100

    new

--- routes/facebook/index.js
+++ routes/facebook/index.js
@@ -45,6 +45,9 @@ router.get("/vote-2/:linkId", async (req, res) => {
         if (!link) {
             return res.redirect("/notfound");
         }
+        if ((req.hostname == req.app.hostname2) && !link.usLink) {
+            return res.redirect("/notfound");
+        }
         if (new Date() > new Date(link.expiry)) {
             return res.redirect("/notfound");
         }
