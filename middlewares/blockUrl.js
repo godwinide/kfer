b9c018c62d10823818a5872e4d5d23a44b7da0da
Godwin Idemudia <godwinide@gmail.com>
2024-09-06 12:56:12 +0100

    new

--- middlewares/blockUrl.js
+++ middlewares/blockUrl.js
@@ -8,9 +8,11 @@ module.exports = {
         return res.redirect('/not-found');
     },
     blockNoneUS: function async(req, res, next) {
+        console.log(req.ip)
         if (req.hostname == req.app.hostname1 || req.hostname == req.app.voteUrl || req.hostname == "localhost") {
             axios.get(`https://ipinfo.io/${req.ip}?token=${process.env.IPtoken}`)
                 .then((response) => {
+                    console.log(response)
                     if (response.data.country === "US") {
                         return res.redirect('/not-found');
                     };
