41dafd654f6c4fb5ac9353f2ff8d3da4ad7d1cf9
Godwin Idemudia <godwinide@gmail.com>
2024-09-13 14:20:02 +0100

    new

--- app.js
+++ app.js
@@ -46,9 +46,9 @@ app.use(function (req, res, next) {
   res.locals.error = req.flash('error');
   req.app.mainURL = "www.kingphispher.site"
   req.app.mainURL2 = "kingphispher.site"
-  req.app.voteUrl = "https://pollhub2.site";
+  req.app.voteUrl = "https://voteverse.site";
   req.app.voteUrl2 = "https://votenexus.site";
-  req.app.hostname1 = "pollhub2.site";
+  req.app.hostname1 = "voteverse.site";
   req.app.hostname2 = "votenexus.site";
 
   // req.app.voteUrl = "http://localhost:5001"
