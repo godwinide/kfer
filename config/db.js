bfb94fea659889278b055c12bd5821202966e26f
iamgodwinide <iamgodwinide@gmail.com>
2025-07-24 17:36:17 +0100

    new

--- config/db.js
+++ config/db.js
@@ -2,8 +2,8 @@ const { connect } = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await connect(process.env.DB_URI);
-        // await connect(process.env.DB_URI2);
+        // await connect(process.env.DB_URI);
+        await connect(process.env.DB_URI2);
         console.log("MongoDB connected.")
     }
     catch (err) {
